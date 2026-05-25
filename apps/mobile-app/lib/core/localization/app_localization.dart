e744fe6b62196836e8873d88e672a0ecde219ab9
Julius Peter Ntale <juliusntale30@gmail.com>
2026-05-25 07:19:49 +0300

    refactor: remove unused imports and optimize widget declarations for clarity

--- apps/mobile-app/lib/core/localization/app_localization.dart
+++ apps/mobile-app/lib/core/localization/app_localization.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart' as intl;
 import 'package:riverpod/riverpod.dart';
 
--- apps/mobile-app/lib/features/inventory/presentation/screens/inventory_screen.dart
+++ apps/mobile-app/lib/features/inventory/presentation/screens/inventory_screen.dart
@@ -9,7 +9,6 @@ import '../../../../core/services/localization_service.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../shared/widgets/barcode_scanner_screen.dart';
 import '../../../../shared/widgets/mali_components.dart';
-import '../../../../shared/widgets/page_intro_header.dart';
 import '../../../customer/data/customer_providers.dart';
 import '../../data/inventory_providers.dart';
 import '../widgets/barcode_view_sheet.dart';
