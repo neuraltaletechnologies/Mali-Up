7a8cce7825a5682b02e5e3d8a5ab1c403bff8e8d
Julius Peter Ntale <juliusntale30@gmail.com>
2026-05-28 22:37:57 +0300

    feat: integrate Sentry for error tracking and update API service configuration

--- apps/mobile-app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/mobile-app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,9 @@ import firebase_storage
 import flutter_secure_storage_macos
 import local_auth_darwin
 import mobile_scanner
+import package_info_plus
 import printing
+import sentry_flutter
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -30,7 +32,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
