91374894f4be36d403f8026e35776d2ca0ffd5d1
Julius Peter Ntale <juliusntale30@gmail.com>
2026-07-10 16:53:07 +0300

    feat: refactor theme management and remove next-themes dependency

--- apps/admin/app/providers.tsx
+++ apps/admin/app/providers.tsx
@@ -6,9 +6,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-        {children}
-      </ThemeProvider>
+      <ThemeProvider>{children}</ThemeProvider>
     </SessionProvider>
   )
 }
